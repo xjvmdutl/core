71b57b9bdf294929c4307829a977b7c7d3e7b0f7
Kimjunho <widn45@naver.com>
2022-02-23 14:13:50 +0900

    day 8 bean Scope(Prototype, Singleton, Request)

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,38 @@
+package hello.core.scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class SingletonTest {
+    @Test
+    public void singletonBeanFind(){
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+            SingletonBean.class);
+
+        SingletonBean singletonBean1 = applicationContext.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = applicationContext.getBean(SingletonBean.class);
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+
+        assertThat(singletonBean1).isSameAs(singletonBean2);
+        applicationContext.close();
+    }
+
+    @Scope("singleton")
+    static class SingletonBean{
+        @PostConstruct
+        public void init(){
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
